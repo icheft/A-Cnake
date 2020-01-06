6f1c3772fc6af3205cb648895c43551eff211f7e
Brian Chen <brian.lxchen@gmail.com>
2020-01-06 14:09:08 +0800

    modify game mode

--- Game.hpp
+++ Game.hpp
@@ -12,7 +12,7 @@
 using namespace sf;
 
 const int SIZE = 15; // default: 15 --> Do not modify
-const int W = 60, H = 50;
+const int W = 60, H = 50; // 60, 50
 const int WIDTH = SIZE * W;
 const int HEIGHT = SIZE * H;
 
