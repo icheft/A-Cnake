e49eca94c5e64a6e0d051ac80b2746f20f419f12
Brian Chen <brian.lxchen@gmail.com>
2020-01-06 14:01:19 +0800

    Change position at the beginning of the game

--- Game.hpp
+++ Game.hpp
@@ -38,20 +38,20 @@ void Snake::init1()
 {
     for(int i = 0; i < SNAKE_MAX_LEN; i++)
     {
-        x[i] = 5;
-        y[i] = H / 2 - 4;
+        x[i] = W - 5;
+        y[i] = H / 2 + 4;
     }
-    dir = right;
+    dir = left;
     len = SNAKE_START_LEN;
 }
 void Snake::init2()
 {
     for(int i = 0; i < SNAKE_MAX_LEN; i++)
     {
-        x[i] = W - 5;
-        y[i] = H / 2 + 4;
+        x[i] = 5;
+        y[i] = H / 2 - 4;
     }
-    dir = left;
+    dir = right;
     len = SNAKE_START_LEN;
 }
 
